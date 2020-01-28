df71c12846bab4f3cf784819b8f514dba00b4767
ediaz <diaz_eda@hotmail.com>
2020-01-27 19:48:46 -0500

    No se puede hacer new de una clase abstracta, error compilacion. No se puede hacer new de una interface, error compilacion.

--- Edgar_Diaz/Abstractas/src/com/polimorfismo/AdminFIgura.java
+++ Edgar_Diaz/Abstractas/src/com/polimorfismo/AdminFIgura.java
@@ -5,17 +5,20 @@ public class AdminFIgura {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Cuadrado  c = new Cuadrado(5, "Cuadrado");
+		Cuadrado c = new Cuadrado(5, "Cuadrado");
 		Triangulo t = new Triangulo(4, 5, "Triangulo");
 		mostrar(c);
 		mostrar(t);
+
+		//Figura f = new Figura(); // No se puede hacer new de una clase abstracta, error compilacion
+		//     Comible c = new Comible(); // No se puede hacer new de una interface, error compilacion
 	}
-	
+
 	public static void mostrar(Figura figura) {
 		System.out.println(figura.getLabel());
 		System.out.println(figura.calcularArea());
-		
-		//label de la figura
+
+		// label de la figura
 		// area de la figura
 	}
 
