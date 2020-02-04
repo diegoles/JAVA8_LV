6633e82449c8e91ca9295437d94be8fbf8855ff2
ediaz <diaz_eda@hotmail.com>
2020-02-03 20:13:51 -0500

    Ejemplos tipo primitivo char

---
+++ TiposDatos/src/com/caracter/EjemploChar.java
@@ -0,0 +1,15 @@
+package com.caracter;
+
+public class EjemploChar {
+
+	public static void main(String[] args) {
+		char c1 = 'a';
+		System.out.println(c1);
+		char c2 = 123;
+		System.out.println(c2); // es una llave {
+
+		int x = c1;
+		System.out.println(x); // 'a'
+	}
+
+}
