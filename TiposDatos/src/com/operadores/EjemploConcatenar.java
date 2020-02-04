828fc9465545008c02512bb24865f25c6cd78e55
ediaz <diaz_eda@hotmail.com>
2020-02-03 20:18:16 -0500

    Concatenación String

---
+++ TiposDatos/src/com/operadores/EjemploConcatenar.java
@@ -0,0 +1,13 @@
+package com.operadores;
+
+public class EjemploConcatenar {
+
+	public static void main(String[] args) {
+
+		System.out.println("valor" + 4 + 5); // valor45
+		System.out.println("valor" + (4 + 5)); // valor9
+		System.out.println("valor" + (4) + (5)); // valor45
+
+	}
+
+}
