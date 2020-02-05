867644f1a95b85814134a10ad1b26fa34c518e99
ediaz <diaz_eda@hotmail.com>
2020-02-05 18:43:01 -0500

    Siempre va al main general con Strings, si compila pero no les para bola.

---
+++ SintaxisMain/src/com/naiki/Test.java
@@ -0,0 +1,20 @@
+package com.naiki;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(Integer[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(int[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
