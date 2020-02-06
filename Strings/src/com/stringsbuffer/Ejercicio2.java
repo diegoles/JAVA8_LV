bf97725fdb164d83d4d1f346f0098781c3c41488
ediaz <diaz_eda@hotmail.com>
2020-02-06 18:25:47 -0500

    Ejemplo StringBuffer constructor con parametros pero no hace nada, crea cadena de logitud 0.

---
+++ Strings/src/com/stringsbuffer/Ejercicio2.java
@@ -0,0 +1,13 @@
+package com.stringsbuffer;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Crea una cadena de logitud 0, puede recibir integer pero no haría nada
+		StringBuffer sf=new StringBuffer(10);  
+		System.out.println(sf);
+		
+	}
+
+}
