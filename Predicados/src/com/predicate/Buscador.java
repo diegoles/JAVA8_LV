036dd5206191e5211cd12762d4077f66036be063
ediaz <diaz_eda@hotmail.com>
2020-02-10 20:02:57 -0500

    removeIf utiliza predicate

--- Predicados/src/com/predicate/Buscador.java
+++ Predicados/src/com/predicate/Buscador.java
@@ -1,5 +1,6 @@
 package com.predicate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
@@ -8,8 +9,11 @@ public class Buscador {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Persona> personas = Arrays.asList(new Persona(10, "Edgar"), new Persona(20, "Juan"),
-				new Persona(30, "Luis"));
+		List<Persona> personas = Arrays.asList(
+				new Persona(10, "Edgar"), 
+				new Persona(20, "Juan"),
+				new Persona(30, "Luis")
+				);
 
 		Persona res = buscar(personas, new MayorEdad());
 		System.out.println(res);
@@ -37,6 +41,27 @@ public class Buscador {
 		
 		Persona res4 = buscar(personas, p -> p.getEdad() > 18);
 		System.out.println(res4);
+		
+		System.out.println(personas);
+		//personas.removeIf(x -> x.getNombre().equalsIgnoreCase("luis"));  // java.lang.UnsupportedOperationException por que es un arrayList
+		System.out.println(personas);
+		
+		List<Persona> people = new ArrayList<>();
+		people.add(new Persona(10, "Pepe") );
+		people.add(new Persona(20, "Lucho"));
+		people.add(new Persona(30, "Juancho"));
+		System.out.println(people);
+		people.removeIf(x -> x.getNombre().equalsIgnoreCase("lucho"));
+		System.out.println(people); // ArrayList tiene sobreescrito el toString, como esta sobreescrito imprime en el formato
+		
+		
+		//List lista; //List<Object> lista;
+		//lista.add(10);
+		//lista.add(new Persona(25, "ED"));
+		//List<Lavable> x; // Lavable es interface
+		//x.add(new Lavable()); // no se puede instanciar
+		//x.add(new Dinero()); //Dinero implementa Lavable
+		
 
 	}
 
